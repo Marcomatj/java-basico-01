7db839440e9c499a2b212bea3477b5011b2b0911
Matuk <mateusmatuk@fai-mg.br>
2020-03-02 20:09:54 -0300

    Quinto commit

--- src/br/fai/projeto01/Main.java
+++ src/br/fai/projeto01/Main.java
@@ -17,6 +17,12 @@ public class Main {
 		System.out.println("A idade e de " + idade+ " anos");
 		System.out.println("Resultado 1: " +((idade + 5)*2));
 		System.out.println("Resultado 2: " +(idade - 5));
+
+		if(idade > 20) {
+		System.out.println("A idade é maior que 20");
+		}else {
+		System.out.println("A idade é menor que 20");	
+		}
 		
 	}
 
