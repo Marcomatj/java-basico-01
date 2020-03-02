aa79c6e36774cee426b1ca23d9233d6b981789d5
Matuk <mateusmatuk@fai-mg.br>
2020-03-02 20:01:18 -0300

    Segundo commit

--- src/br/fai/projeto01/Main.java
+++ src/br/fai/projeto01/Main.java
@@ -3,8 +3,33 @@ package br.fai.projeto01;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Main app = new Main();
+		app.start();
+		
 
 	}
 
+	private void start() {
+		
+		int idade = 15;
+		
+		System.out.println("A idade e de " + idade+ " anos");
+		
+		
+		
+	}
+
 }
+
+
+/*Fazer um programa que, a partir de uma idade já declarada (15), faça:
+exiba a idade:
+A idade é de xx anos
+somar 5 nessa idade e multiplicar por 2 - exibir na tela:
+Resultado 1: xx
+subtrair 5 - exibir na tela:
+Resultado 2: xx
+Verificar SE a idade é maior do que 20 - exibir na tela:
+A idade é maior que 20 ou A idade é menor que 20
+*/
