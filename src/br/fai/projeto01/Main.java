ddf0158de5be2b3c2c38d01563d3c978872a2063
Matuk <mateusmatuk@fai-mg.br>
2020-03-02 20:08:19 -0300

    Quarto commit

--- src/br/fai/projeto01/Main.java
+++ src/br/fai/projeto01/Main.java
@@ -16,19 +16,9 @@ public class Main {
 		
 		System.out.println("A idade e de " + idade+ " anos");
 		System.out.println("Resultado 1: " +((idade + 5)*2));
+		System.out.println("Resultado 2: " +(idade - 5));
 		
 	}
 
 }
 
-
-/*Fazer um programa que, a partir de uma idade já declarada (15), faça:
-exiba a idade:
-A idade é de xx anos
-somar 5 nessa idade e multiplicar por 2 - exibir na tela:
-Resultado 1: xx
-subtrair 5 - exibir na tela:
-Resultado 2: xx
-Verificar SE a idade é maior do que 20 - exibir na tela:
-A idade é maior que 20 ou A idade é menor que 20
-*/
